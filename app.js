80e830b5481eb8b81b04c777b6e2be6e6dbea512
Mian A Shabbir <mian78640@gmail.com>
2019-09-24 02:19:25 -0400

    setup controllers for app

---
+++ app.js
@@ -0,0 +1,15 @@
+var budgetController = (function() {
+
+})();
+
+
+var UIController = (function() {
+
+})();
+
+
+var appController = (function(budget, UI) {
+    document.querySelector('#add-btn').addEventListener('click',function() {
+        
+        });
+})(budgetController, UIController);
